9105577a41e431993cebea18a550f4ccc19b07b4
fransappia01 <fransappia01@gmail.com>
2020-05-20 00:46:48 -0300

    se realizo ejercicio de convertir unidades

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -9,3 +9,16 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+function CambiarUnidades(id, valor) {
+    if (isNaN(valor)){
+        alert("Se ingreso un valor invalido");
+        document.lasUnidades.unid_metro.value = "";
+        document.lasUnidades.unid_pulgada.value = "";
+        document.lasUnidades.unid_pie.value = "";
+        document.lasUnidades.unid_yarda.value= "";
+    }else if (id=="metro"){
+        document.lasUnidades.unid_pulgada.value = 39.3701*valor;
+        document.lasUnidades.unid_pie.value = 3.28084*valor;
+        document.lasUnidades.unid_yarda.value= 1.09361*valor;
+    }
+}
