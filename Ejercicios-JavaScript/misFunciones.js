7f0e4cb5af0a76eb0216faeca68a0af3ced3f192
fransappia01 <fransappia01@gmail.com>
2020-05-20 01:20:19 -0300

    se realizo ejercicio de grados a radianes

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -22,3 +22,17 @@ function CambiarUnidades(id, valor) {
         document.lasUnidades.unid_yarda.value= 1.09361*valor;
     }
 }
+
+function convertirGR(id) {
+    var grad, rad;
+    if (id=="grados"){
+        grad=document.getElementById("grados").value;
+        rad = (grad*Math.PI)/180;
+
+    }else if (id=="radianes"){
+        rad=document.getElementById("radianes").value;
+        grad=(rad*180)/ Math.PI
+    }
+    document.getElementById("grados").value=grad;
+    document.getElementById("radianes").value=rad;
+}
