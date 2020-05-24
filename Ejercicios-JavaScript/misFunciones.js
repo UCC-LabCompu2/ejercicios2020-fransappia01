54f3c388a439ddd345b5763d9a8f20edf083cca3
fransappia01 <fransappia01@gmail.com>
2020-05-24 20:45:47 -0300

    ejercicos innerHTML

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -80,7 +80,7 @@ function CambiarUnidades(id, valor) {
 
         num1 = document.getElementsByName("sum_num1")[0].value;
         num2 = document.getElementsByName("sum_num2")[0].value;
-        document.getElementsByName("sum_total")[0].value = Number(num1) + Number(num2);
+        document.getElementsByName("sum_total")[0].innerHTML = Number(num1) + Number(num2);
     }
 
     function calcularResta() {
@@ -88,7 +88,7 @@ function CambiarUnidades(id, valor) {
 
         num1 = document.getElementsByName("res_num1")[0].value;
         num2 = document.getElementsByName("res_num2")[0].value;
-        document.getElementsByName("res_total")[0].value = Number(num1) - Number(num2);
+        document.getElementsByName("res_total")[0].innerHTML = Number(num1) - Number(num2);
 
     }
 
@@ -97,7 +97,7 @@ function CambiarUnidades(id, valor) {
 
         num1 = document.getElementsByName("mul_num1")[0].value;
         num2 = document.getElementsByName("mul_num2")[0].value;
-        document.getElementsByName("mul_total")[0].value = Number(num1) * Number(num2);
+        document.getElementsByName("mul_total")[0].innerHTML = Number(num1) * Number(num2);
 
     }
 
@@ -106,6 +106,6 @@ function CambiarUnidades(id, valor) {
 
         num1 = document.getElementsByName("div_num1")[0].value;
         num2 = document.getElementsByName("div_num2")[0].value;
-        document.getElementsByName("div_total")[0].value = Number(num1) / Number(num2);
+        document.getElementsByName("div_total")[0].innerHTML = Number(num1) / Number(num2);
 
     }
