f1fa9b6db24ae7b80d8aefdaaf2cbb2934de7340
fransappia01 <fransappia01@gmail.com>
2020-05-24 23:47:26 -0300

    se realizo ejercicio de dibujar cuadrado y circulo canvas

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -130,3 +130,19 @@ function cargarResultado() {
     document.getElementById("dist").value = can + " " + un;
 
 }
+
+function DibujarCirCuad() {
+
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var xMax = canvas.width;
+    var yMax = canvas.height;
+    var margen = 5;
+    ctx.fillStyle = "#cb0d12";
+    ctx.fillRect(0+margen,yMax-40-margen, 40, 40);
+
+    ctx.arc(xMax/2, yMax/2, 20, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.fillStyle = "#344389";
+    ctx.fill();
+}
