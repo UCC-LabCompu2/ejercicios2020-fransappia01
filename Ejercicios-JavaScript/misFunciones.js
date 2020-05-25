049bea9614a87420bb6415bf3b585499dd8956b6
fransappia01 <fransappia01@gmail.com>
2020-05-25 01:12:57 -0300

    se realizo ejercicio de cuadriculado

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -174,3 +174,54 @@ function limpiarCanvas() {
 
     canvas.width = canvas.width;
 }
+
+
+
+function dibujarCuadriculado() {
+
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    var anchoMax = canvas.width;
+    var alturaMax = canvas.height;
+
+    // Dibujar lineas horizontales
+    ctx.beginPath();
+    for (var i=0;i<alturaMax;){
+    ctx.moveTo(0, i);
+    ctx.lineTo(anchoMax, i);
+    ctx.strokeStyle = "#0cf804";
+    ctx.stroke();
+    i = i+20;
+    }
+    ctx.closePath();
+
+    //Dinujar lineas verticales
+    ctx.beginPath();
+    for (var i=0;i<anchoMax;){
+        ctx.moveTo(i, 0);
+        ctx.lineTo(i, alturaMax);
+        ctx.strokeStyle = "#0cf804";
+        ctx.stroke();
+        i = i+20;
+    }
+    ctx.closePath();
+
+    //eje x
+    ctx.beginPath();
+
+    ctx.moveTo(0, alturaMax/2);
+    ctx.lineTo(anchoMax, alturaMax/2);
+        ctx.strokeStyle = "#05084e";
+        ctx.stroke();
+    ctx.closePath();
+
+    //eje y
+    ctx.beginPath();
+
+    ctx.moveTo(anchoMax/2, 0);
+    ctx.lineTo (anchoMax/2, alturaMax);
+    ctx.strokeStyle = "#4e0507";
+    ctx.stroke();
+    ctx.closePath();
+}
