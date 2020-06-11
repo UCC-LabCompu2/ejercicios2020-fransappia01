c77e3ad4761c4943853a417fa898a6488ad299b0
fransappia01 <fransappia01@gmail.com>
2020-06-11 11:56:18 -0300

    creo otra pagina

--- Ejercicios-CSS/funciontest.js
+++ Ejercicios-CSS/funciontest.js
@@ -1,4 +1,7 @@
 const contenedor = document.getElementById("test");
+const botonRes = document.getElementById("boton");
+const resultadoTest = document.getElementById("resultado");
+
 
 const preguntas = [
 
@@ -10,7 +13,42 @@ const preguntas = [
             b: "Celular Solar System",
             c: "Casette Solarium Silvester"
         },
-        respuestaCorrecta: "a"
+        respuestasCorrecta: "a"
+
+    },
+    {
+        pregunta: "2.Que servidores de Git conoces?",
+        respuestas: {
+
+            a: "Github",
+            b: "GitLab",
+            c: "Gitroom"
+        },
+        respuestasCorrecta: "a"
+    },
+    {
+
+        pregunta: "2.Que servidores de Git conoces?",
+        respuestas: {
+
+            a: "Github",
+            b: "GitLab",
+            c: "Gitroom"
+        },
+        respuestasCorrecta: "a"
+
+    },
+    {
+        pregunta: "3.Que comandos de Git conoces?",
+        respuestas: {
+
+            a: "Clone",
+            b: "Init",
+            c: "Cout"
+        },
+        respuestasCorrecta: "a"
+
+
     }
     
 ]
@@ -45,4 +83,43 @@ function mostrarTest() {
 
 }
 
-mostrarTest()
+mostrarTest();
+
+function mostrarResultado() {
+    const respuestas = contenedor.querySelectorAll(".respuestas");
+    let respuestasCorrecta = 0;
+
+    preguntas.forEach((preguntaActual, numeroDePregunta) => {
+        const todasLasRespuestas = respuestas[numeroDePregunta];
+        const checkboxRespuestas  = `input[name='${numeroDePregunta}']:checked`;
+        const RespuestaElegida = (todasLasRespuestas.querySelector(checkboxRespuestas) || {}).value;
+
+
+        if (RespuestaElegida === preguntaActual.respuestasCorrecta){
+
+            respuestasCorrecta++;
+
+            respuestas[numeroDePregunta].style.color = 'blue';
+        }else{
+
+            respuestas[numeroDePregunta].style.color = 'red';
+        }
+
+
+    });
+
+    resultadoTest.innerHTML = 'Ha acertado ' + respuestasCorrecta + 'preguntas de un total de' + preguntas.length;
+
+
+}
+
+botonRes.addEventListener('click', mostrarResultado);
+
+
+function cargarPagina(url) {
+
+
+    url = "http://localhost:63342/ejercicios2020-fransappia01/Ejercicios-CSS/TP%20LAB%20PAGINA%20DOS.html?_ijt=pmct48f9mns46dmluioqu4got" ;
+
+    window.open(url);
+}
