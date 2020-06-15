be9d9cc43cccc56a3d358f72a243826573caea90
fransappia01 <fransappia01@gmail.com>
2020-06-14 22:42:58 -0300

    realizando tp

--- Ejercicios-CSS/funciontest.js
+++ Ejercicios-CSS/funciontest.js
@@ -1,125 +1,77 @@
-const contenedor = document.getElementById("test");
-const botonRes = document.getElementById("boton");
-const resultadoTest = document.getElementById("resultado");
+var res=new Array;
+var no=new Array;
+var tures=new Array;
+var explicares=new Array;
+var calif=0;
 
+res[1]="a";//res = respuesta
+res[2]="a";
+res[3]="c";
+res[4]="b";
+res[5]="a";
 
-const preguntas = [
+explicares[1]="Mal,la respuesta correcta es a";
+explicares[2]="Mal,la respuesta correcta es a";
+explicares[3]="Mal,la respuesta correcta es c";
+explicares[4]="Mal,la respuesta correcta es b";
+explicares[5]="Mal,la respuesta correcta es a";
 
-    {
-        pregunta: "1.Que es significan las siglas de CCS?",
-        respuestas: {
-
-            a: "Cascading Style Sheets",
-            b: "Celular Solar System",
-            c: "Casette Solarium Silvester"
-        },
-        respuestasCorrecta: "a"
-
-    },
-    {
-        pregunta: "2.Que servidores de Git conoces?",
-        respuestas: {
-
-            a: "Github",
-            b: "GitLab",
-            c: "Gitroom"
-        },
-        respuestasCorrecta: "a"
-    },
-    {
-
-        pregunta: "2.Que servidores de Git conoces?",
-        respuestas: {
-
-            a: "Github",
-            b: "GitLab",
-            c: "Gitroom"
-        },
-        respuestasCorrecta: "a"
-
-    },
-    {
-        pregunta: "3.Que comandos de Git conoces?",
-        respuestas: {
-
-            a: "Clone",
-            b: "Init",
-            c: "Cout"
-        },
-        respuestasCorrecta: "a"
-
-
+function Resp(question,answer){
+    tures[question]=answer;
+}
+function Score(){
+    var answertext="RESULTADOS\n";
+    calif=0;
+    for(i=1;i<=5;i++){
+        answertext=answertext+"\nPregunta"+i+".";
+        if(res[i]!==tures[i]){
+            answertext=answertext+explicares[i]+"\n";
+        }
+        else{
+            answertext=answertext+"bien\n";
+            calif++;
+        }}
+    answertext=answertext+"\nCalificacion:"+calif;
+    answertext=answertext+"\nDiagnostico:";
+    if(calif>=0){
+        answertext=answertext+"Mal";
     }
-    
-]
-
-function mostrarTest() {
-    const preguntasYrespuestas = [];
-
-    preguntas.forEach((preguntaActual, numeroDePregunta) => {
-
-        const respuestas = [];
-        for (letraRespuesta in preguntaActual.respuestas){
-            respuestas.push(
-                `<label>
-                    <input type="radio" name="${numeroDePregunta}" value="${letraRespuesta}">
-                    ${letraRespuesta} : ${preguntaActual.respuestas[letraRespuesta]}
-                
-                </label>`
-            );
-
-        }
-        preguntasYrespuestas.push(
-            `<div class="cuestion"> ${preguntaActual.pregunta}</div>
-             <div class="respuestas"> ${respuestas.join('')}</div>
-
-            
-            `
-
-        );
-    });
-
-    contenedor.innerHTML = preguntasYrespuestas.join('');
-
-}
-
-mostrarTest();
-
-function mostrarResultado() {
-    const respuestas = contenedor.querySelectorAll(".respuestas");
-    let respuestasCorrecta = 0;
-
-    preguntas.forEach((preguntaActual, numeroDePregunta) => {
-        const todasLasRespuestas = respuestas[numeroDePregunta];
-        const checkboxRespuestas  = `input[name='${numeroDePregunta}']:checked`;
-        const RespuestaElegida = (todasLasRespuestas.querySelector(checkboxRespuestas) || {}).value;
+    if(calif>=1&&calif<=2){
+        answertext=answertext+"Regular";
+    }
+    if(calif>=3&&calif<=4){
+        answertext=answertext+"Bueno";
+    }
+    if(calif>=5){
+        answertext=answertext+"Excelente";
+    }
+    alert(answertext);}
 
 
-        if (RespuestaElegida === preguntaActual.respuestasCorrecta){
 
-            respuestasCorrecta++;
+function animarFoto() {
 
-            respuestas[numeroDePregunta].style.color = 'blue';
-        }else{
+    var canvas = document.getElementById("myCanvas")
+    var ctx = canvas.getContext("2d")
 
-            respuestas[numeroDePregunta].style.color = 'red';
-        }
+    canvas.width = canvas.width;
 
 
-    });
-
-    resultadoTest.innerHTML = 'Ha acertado ' + respuestasCorrecta + 'preguntas de un total de' + preguntas.length;
-
-
-}
-
-botonRes.addEventListener('click', mostrarResultado);
+    var img = new Image();
+    img.src = "images/auto.png ";
 
 
-function cargarPagina(url) {
+    img.onload = function(){
+        ctx.drawImage(img,x, 100)
+    }
+
+    if (x>canvas.width){
+
+        x=0
+    }
+    x+=dx;
 
 
-    url = "http://localhost:63342/ejercicios2020-fransappia01/Ejercicios-CSS/TP%20LAB%20PAGINA%20DOS.html?_ijt=pmct48f9mns46dmluioqu4got" ;
 
-    window.open(url);
+
 }
